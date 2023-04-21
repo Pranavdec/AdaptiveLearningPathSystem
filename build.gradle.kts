9dfc4ba3f7cd5f63918efa41f7c9fbe62a34ae72
abcd <pallerlapranavdec27@gmail.com>
2023-04-21 13:01:37 +0530

    Change in json version in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("org.json:json:20210307")
+    implementation("org.json:json:20220320")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
